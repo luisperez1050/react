cf511e42d7c44fa2ddd9c57ab81fb71cfabefc94
Luis Perez <luisperez1050@outlook.com>
2020-09-09 06:55:51 -0400

    testing deployment

---
+++ card-game/build/precache-manifest.b79cd25882c51782167ead80e6af2313.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7fff5ddc4bddd2dcd10f3a0d1c6e5a",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "f9ad1caa0383f434a443",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b80371027c14b4c85ae",
+    "url": "/react/static/js/2.afdc64fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.afdc64fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ad1caa0383f434a443",
+    "url": "/react/static/js/main.ccb5f268.chunk.js"
+  },
+  {
+    "revision": "19a46bf7c04a07e5ad3d",
+    "url": "/react/static/js/runtime-main.7bc8be5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
