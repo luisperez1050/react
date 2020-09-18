a9a8659936f71d24acd137c4255cdf0c3be22d0b
Luis Perez <luisperez1050@outlook.com>
2020-09-17 22:31:08 -0400

    clean up going to change this react sample page

--- card-game/src/components/HeaderNav.js
+++ card-game/src/components/HeaderNav.js
@@ -11,12 +11,6 @@ const HeaderNav = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="https://luisperez1050.github.io/index.html">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="https://luisperez1050.github.io/card_game.html">Card Game</a>
-                        </li>
-                        <li className="nav-item">
                             <a className="nav-link" href="/react">React Card Game <span className="sr-only">(current)</span></a>
                         </li>
                     </ul>
